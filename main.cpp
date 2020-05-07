32d325ebdd326c254d1f2125ff7f9bb1c4427ef6
Sergei-Kichigin <sergei.kichigin.01@mail.ru>
2020-05-07 22:16:58 +0700

    Create new line in code

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include <cstdlib>
 using namespace std;
 
-int main() 
-{ 
-    cout << "Hello, world!";
-    return 0; 
+int main()
+{
+    cout << "Hello, world!\n"
+         << "it was funny";
+    return 0;
 }
